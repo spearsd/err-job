b346723a0bf3f86776bda70835693ff6e0e29883
Dustin <dustin.spears@live.com>
2017-10-31 13:20:47 -0400

    Update job_status.py

--- job_status.py
+++ job_status.py
@@ -6,4 +6,5 @@ class AutoSysJob(BotPlugin):
     @botcmd
     def job_status(self, msg, args):
         """Return job status"""
-        return "Job Name!"
+        string = "Job Name                                                         Last Start           Last End             ST Run/Ntry Pri/Xit" + "\n________________________________________________________________ ____________________ ____________________ __ ________ _______" + "\nAZ7#cmd#UIHealthCheckCMD                                         10/28/2017 22:35:03  10/28/2017 22:35:52  SU 157897088/1 0"
+        return string
