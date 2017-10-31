7ff1c0b4b4a60ce54e59e7955b94a769b9d95129
Dustin <dustin.spears@live.com>
2017-10-31 15:16:15 -0400

    Update job_status.py

--- job_status.py
+++ job_status.py
@@ -9,15 +9,15 @@ class AutoSysJob(BotPlugin):
         """Return job status"""
         string = ""
         string_multi = ""
-        with tempfile.TemporaryFile() as tempf:
-            proc = subprocess.Popen(['ls','-l'], stdout=tempf)
-            proc.wait()
-            tempf.seek(0)
-            string = str(string) + str(tempf.read())
-        string_array = string.split("\\n")
-        for s in string_array:
-            string_multi = string_multi + s
-        return "```" + string_multi
+        #with tempfile.TemporaryFile() as tempf:
+        #    proc = subprocess.Popen(['ls','-l'], stdout=tempf)
+        #    proc.wait()
+        #    tempf.seek(0)
+        #    string = str(string) + str(tempf.read())
+        #string_array = string.split("\\n")
+        #for s in string_array:
+        #    string_multi = string_multi + s
+        #return "```" + string_multi
         #return "```" + re.sub(r"\\", "\\\\", str(string))
-        #string = "Job Name                                                         Last Start           Last End             ST Run/Ntry Pri/Xit" + "\n________________________________________________________________ ____________________ ____________________ __ ________ _______" + "\nAZ7#cmd#UIHealthCheckCMD                                         10/28/2017 22:35:03  10/28/2017 22:35:52  SU 157897088/1 0"
-        #return string
+        string = "Job Name                                                         Last Start           Last End             ST Run/Ntry Pri/Xit" + "\n________________________________________________________________ ____________________ ____________________ __ ________ _______" + "\nAZ7#cmd#UIHealthCheckCMD                                         10/28/2017 22:35:03  10/28/2017 22:35:52  SU 157897088/1 0"
+        return "```" + string
