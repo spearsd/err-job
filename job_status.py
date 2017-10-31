9fa41d8e46f17b839c9d001d3a72096f069b09e8
Dustin <dustin.spears@live.com>
2017-10-31 14:40:32 -0400

    Update job_status.py

--- job_status.py
+++ job_status.py
@@ -1,6 +1,5 @@
 from errbot import BotPlugin, botcmd
-import subprocess
-import tempfile
+import subprocess, tempfile, re
 
 class AutoSysJob(BotPlugin):
     """AutoSys job plugin for Errbot"""
@@ -14,6 +13,6 @@ class AutoSysJob(BotPlugin):
             proc.wait()
             tempf.seek(0)
             string = str(string) + str(tempf.read())
-        return "```" + str(string)
+        return "```" + re.sub(r"\\", "\\\\", str(string))
         #string = "Job Name                                                         Last Start           Last End             ST Run/Ntry Pri/Xit" + "\n________________________________________________________________ ____________________ ____________________ __ ________ _______" + "\nAZ7#cmd#UIHealthCheckCMD                                         10/28/2017 22:35:03  10/28/2017 22:35:52  SU 157897088/1 0"
         #return string
