4f7482a3b2d308711a5c183e7588a70783ac0356
Dustin <dustin.spears@live.com>
2017-10-31 15:21:33 -0400

    Update job_status.py

--- job_status.py
+++ job_status.py
@@ -9,6 +9,8 @@ class AutoSysJob(BotPlugin):
         """Return job status"""
         string = ""
         string_multi = ""
+        yield args
+        yield msg
         #with tempfile.TemporaryFile() as tempf:
         #    proc = subprocess.Popen(['ls','-l'], stdout=tempf)
         #    proc.wait()
@@ -19,5 +21,5 @@ class AutoSysJob(BotPlugin):
         #    string_multi = string_multi + s
         #return "```" + string_multi
         #return "```" + re.sub(r"\\", "\\\\", str(string))
-        string = "Job Name                                                           Last Start           Last End             ST Run/Ntry Pri/Xit" + "\n____________________________________________________________________________________________ ____________________ ____________________ __ ________ _______" + "\nAZ7#cmd#UIHealthCheckCMD                                         10/28/2017 22:35:03  10/28/2017 22:35:52  SU 157897088/1 0"
+        string = "Job Name                                                           Last Start           Last End             ST Run/Ntry Pri/Xit" + "\n____________________________________________________________________________________________ ____________________ ____________________ __ ________ _______" + "\n" + "AZ7#cmd#UIHealthCheckCMD                                         10/28/2017 22:35:03  10/28/2017 22:35:52  SU 157897088/1 0"
         return "```" + string
