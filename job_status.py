74b41ac23a60e870b9a1836d2fd6ac608a490380
Dustin <dustin.spears@live.com>
2017-10-31 12:05:01 -0400

    Create job_status.py

---
+++ job_status.py
@@ -0,0 +1,9 @@
+from errbot import BotPlugin, botcmd
+
+class AutoSysJob(BotPlugin):
+    """AutoSys job plugin for Errbot"""
+
+    @botcmd
+    def job_status(self, msg, args):
+        """Return job status"""
+        return "Job Status!"
