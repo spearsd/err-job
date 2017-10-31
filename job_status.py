dccbe607c2656c386b842c1d2657d869b2a32537
Dustin <dustin.spears@live.com>
2017-10-31 14:49:19 -0400

    Update job_status.py

--- job_status.py
+++ job_status.py
@@ -13,6 +13,9 @@ class AutoSysJob(BotPlugin):
             proc.wait()
             tempf.seek(0)
             string = str(string) + str(tempf.read())
-        return "```" + re.sub(r"\\", "\\\\", str(string))
+        string_array = string.split("\\n")
+        for str in string_array:
+            yield "```" + str
+        #return "```" + re.sub(r"\\", "\\\\", str(string))
         #string = "Job Name                                                         Last Start           Last End             ST Run/Ntry Pri/Xit" + "\n________________________________________________________________ ____________________ ____________________ __ ________ _______" + "\nAZ7#cmd#UIHealthCheckCMD                                         10/28/2017 22:35:03  10/28/2017 22:35:52  SU 157897088/1 0"
         #return string
