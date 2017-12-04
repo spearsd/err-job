63fcb011269fe1b7d36bb4afdbad93d27a4e4228
Dustin <dustin.spears@live.com>
2017-12-04 10:46:46 -0500

    Update and rename job_status.py to job.py

--- job.py
+++ job.py
@@ -1,5 +1,5 @@
 from errbot import BotPlugin, botcmd
-import subprocess, tempfile, re
+import subprocess, tempfile, re, time
 
 class AutoSysJob(BotPlugin):
     """AutoSys job plugin for Errbot"""
@@ -22,3 +22,23 @@ class AutoSysJob(BotPlugin):
         #return "```" + re.sub(r"\\", "\\\\", str(string))
         string = "Job Name                                                           Last Start           Last End             ST Run/Ntry Pri/Xit" + "\n____________________________________________________________________________________________ ____________________ ____________________ __ ________ _______" + "\n" + job_name + "                                         10/28/2017 22:35:03  10/28/2017 22:35:52  SU 157897088/1 0"
         return string
+    
+    @botcmd
+    def job_start(self, msg, args):
+        """Return job status"""
+        string = ""
+        string_multi = ""
+        job_name = args
+        #with tempfile.TemporaryFile() as tempf:
+        #    proc = subprocess.Popen(['ls','-l'], stdout=tempf)
+        #    proc.wait()
+        #    tempf.seek(0)
+        #    string = str(string) + str(tempf.read())
+        #string_array = string.split("\\n")
+        #for s in string_array:
+        #    string_multi = string_multi + s
+        #return "```" + string_multi
+        #return "```" + re.sub(r"\\", "\\\\", str(string))
+        yield "Starting " + job_name + "..."
+        time.sleep(2)
+        yield job_name + " has started."
