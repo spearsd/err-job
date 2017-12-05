bca60884f88f8d747c5b8335d59dddc4cd96108d
Dustin <dustin.spears@live.com>
2017-12-05 13:47:43 -0500

    Update job.py

--- job.py
+++ job.py
@@ -18,7 +18,7 @@ class AutoSysJob(BotPlugin):
         #t = PrettyTable(['Job Name', 'Last Start', 'Last End', 'ST', 'Run/Ntry', 'Pri/Xit'])
         #t.add_row([args, '10/28/2017 22:35:03', '10/28/2017 22:35:52', 'SU', '157897088/1', '0'])
         #string = "Job Name                                                           Last Start           Last End             ST Run/Ntry Pri/Xit" + "\n____________________________________________________________________________________________ ____________________ ____________________ __ ________ _______" + "\n" + job_name + "                                         10/28/2017 22:35:03  10/28/2017 22:35:52  SU 157897088/1 0"
-        string = "Job Name:   \t" + job_name
+        string = "Job Name:  \t" + job_name
         string += "\nLast Start: \t" + "10/28/2017 22:35:03"
         string += "\nLast End: \t\t" + "10/28/2017 22:35:52"
         string += "\nST: \t\t\t\t" + "SU"
